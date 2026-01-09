2217b6dfab27d738a459b1927e1ed0e21520051a
Svardsten53 <62435779+Svardsten53@users.noreply.github.com>
2026-01-09 21:51:23 +0100

    Slovenia added

---
+++ slovenia/Translate/egnafilerorg/links.php
@@ -0,0 +1,57 @@
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style>
+.servicecontrols button {
+  border: 2px solid black;
+  background-color: rgb(219, 255, 235);
+  padding: 5px;
+  width: 30%;
+}
+</style>
+<br>
+<h3>Program-related links</h3>
+<div class="servicecontrols">
+  <form action="https://github.com/Nachtzuster/BirdNET-Pi" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">Program source</button>
+  </form>
+  <form action="https://github.com/mcguirepr89/BirdNET-Pi/wiki" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">Wiki help page</button>
+  </form>
+  <form action="https://birdnet.cornell.edu" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">BirdNET</button>
+  </form>
+  <form action="https://birdnetpi.com" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">BirdNET-Pi</button>
+  </form>
+  <form action="https://app.birdweather.com" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">BirdWeather</button>
+  </form>
+</div>
+<br>
+<h3>Own links</h3>
+<div class="servicecontrols">
+  <form action="https://www.michaelfogleman.com/birds/?lat=59.329323&lng=18.068581&radius_mi=25&month=12" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">Bird Finder</button>
+  </form>
+  <form action="https://birdtunes.net/?lang=sv" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">BirdTunes</button>
+  </form> 
+  <form action="" method="GET" id="views" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="view" value="QA" form="views">Questions and answers about BirdNET-Pi (English)</button>
+  </form>
+</div>
+<br>
+<h3>Other BirdNET-Pi</h3>
+<div class="servicecontrols">
+  <form action="https://birdnet.svardsten.se" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">Tyresö, Öringe</button>
+  </form> 
+  <form action="https://lobirds.ddns.net" target="_blank" method="get" style="display: flex; justify-content: center; margin: 10px 0;">
+    <button type="submit" name="submit">Landsort Bird Observatory</button>
+  </form>
+  <br><br><br>
+<div style="text-align: center;">
+  Please email if you have any questions or would like to do your own installation.<br>
+  birdnetpi@svardsten.com
+</div>
